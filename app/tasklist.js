ca8693d52bc0dd7e1847114d8a67411558be5576
akuloloia <akuloloi@hawaii.edu>
2016-10-09 10:53:03 -1000

    Task 4: Tasklist authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,9 +1,3 @@
-/**
- *
- *
- * Created by Tony on 10/9/2016.
- */
-
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
@@ -12,8 +6,39 @@ if (Meteor.isClient) {
       return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
+
+  Template.tasks.events({
+    "submit .add-task": function(event) {
+      let name = event.target.name.value;
+      Meteor.call('addTask', name);
+      event.target.name.value = '';
+      return false;
+    },
+
+    "click .delete-task": function(event) {
+      if (confirm('Delete Task?')) {
+        Meteor.call('deleteTask', this._id);
+      }
+      return false;
+    }
+  })
 }
 
 if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  addTask: function(name){
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('No Access');
+    }
+    Tasks.insert({
+      name: name, createdAt: new Date(), userId: Meteor.userId()
+    });
+  },
+
+  deleteTask: function(taskId) {
+    Tasks.remove(taskId);
+  }
+})
